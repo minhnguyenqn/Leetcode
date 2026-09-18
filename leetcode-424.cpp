5d7005ded3eb03ff13856e47195a8cd24e2d3d37
Minh <minhnguyenleqn07@gmail.com>
2026-09-19 00:03:33 +0700

    okok

--- leetcode-424.cpp
+++ leetcode-424.cpp
@@ -8,3 +8,22 @@ you can get after performing the above operations.*/
 #include <algorithm>
 #include <climits>
 using namespace std;
+
+int longestsubstring(string s, int k){
+    int left=0, right=0;
+    int freq[26]={0};
+    int maxfreq=0;
+    int maxLen=0;
+    while (right<s.length()){
+        char ch=s[right];
+        freq[ch-'A']++;
+        maxfreq=max(maxfreq,freq[ch-'A']);
+        if ((right - left + 1) - maxfreq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+            maxLen = max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
+    }
